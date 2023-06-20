20bd1e5c3f0a566b3557f9ea2d487252cf633786
ChappaAbhiram <abhiramchappa@gmail.com>
2023-06-21 01:09:26 +0530

    all are bold and third element background green

--- index.js
+++ index.js
@@ -5,8 +5,15 @@
 //console.log(document.all);
 //console.log(document.all[10]);
 //console.log(document.forms);
-var headertitle = document.getElementById('header-title');
-headertitle.style.border = 'solid 2px black';
-var addItems = document.getElementsByClassName('title');
-addItems[0].style.fontWeight = 'bold';
-addItems[0].style.color = 'green';
+//var headertitle = document.getElementById('header-title');
+//headertitle.style.border = 'solid 2px black';
+//var addItems = document.getElementsByClassName('title');
+//addItems[0].style.fontWeight = 'bold';
+//addItems[0].style.color = 'green';
+var items = document.getElementsByClassName('list-group-item');
+console.log(items);
+
+items[2].style.backgroundColor = 'green';
+for(var i=0;i<items.length;i++){
+items[i].style.fontWeight = 'bold';
+}
